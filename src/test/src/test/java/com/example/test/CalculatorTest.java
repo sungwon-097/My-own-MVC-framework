9fceb6a8f6b9d66d2f4b0dbc4fda7192818a4507
sungwon-097 <csw2757@gmail.com>
2022-09-24 19:48:36 +0900

    #12 사칙연산 구현
    1. Argument에 인자 직접 주입 방식
    2. Enum class인 ArithmeticOperator에 위임
    3. calculate/ 패키지의 구현체를 참조하는 NewArithmeticOperator에 위임
    
    + 유효성 확인

--- src/test/src/test/java/com/example/test/CalculatorTest.java
+++ src/test/src/test/java/com/example/test/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.example.test;
 
+import com.example.test.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -10,30 +11,32 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CalculatorTest {
     
-    @DisplayName("덧셈 연산을 수행한다")
-    @Test
-    void additionTest(){
-        int result = Calculator.calculate(1,"+",2);
-        assertThat(result).isEqualTo(3);
-    }
-
-    @DisplayName("뺄셈 연산을 수행한다")
-    @Test
-    void subtractionTest(){
-        int result = Calculator.calculate(1,"-",2);
-        assertThat(result).isEqualTo(-1);
-    }
+//    @DisplayName("덧셈 연산을 수행한다")
+//    @Test
+//    void additionTest(){
+//        int result = Calculator.calculate(1,"+",2);
+//        assertThat(result).isEqualTo(3);
+//    }
+//
+//    @DisplayName("뺄셈 연산을 수행한다")
+//    @Test
+//    void subtractionTest(){
+//        int result = Calculator.calculate(1,"-",2);
+//        assertThat(result).isEqualTo(-1);
+//    }
 
     @DisplayName("연산을 수행한다")
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void calculateTest(int operand1, String operator, int operand2, int result){
-        int calculateResult = Calculator.calculate(operand1,operator,operand2);
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1),operator,new PositiveNumber(operand2));
+        // calculator 에게 작업을 위임함
         assertThat(calculateResult).isEqualTo(result);
     }
 
@@ -45,4 +48,10 @@ public class CalculatorTest {
                 arguments(1,"/",2,0)
         );
     }
+//    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다") // PositiveNumber 로 해결
+//    @Test
+//    void calculateExceptionTest(){
+//        assertThatCode(()->Calculator.calculate(new PositiveNumber(10), "/", new PositiveNumber(0)))
+//                .isInstanceOf(IllegalArgumentException.class);
+//    }
 }
