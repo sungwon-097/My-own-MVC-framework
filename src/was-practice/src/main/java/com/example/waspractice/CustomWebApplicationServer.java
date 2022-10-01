b7121e40330d003321fa67ef9ad7dcbf68bbc96e
sungwon-097 <csw2757@gmail.com>
2022-10-01 17:16:54 +0900

    #21 was의 역할 알아보기
    ClientSocket의 InputStream()으로 받은 내용을 해석해 Spring으로 보내준다

--- src/was-practice/src/main/java/com/example/waspractice/CustomWebApplicationServer.java
+++ src/was-practice/src/main/java/com/example/waspractice/CustomWebApplicationServer.java
@@ -3,9 +3,10 @@ package com.example.waspractice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
 
@@ -25,6 +26,20 @@ public class CustomWebApplicationServer {
 
             while((clientSocket = serverSocket.accept()) != null){ // 해당 port 로 serverSocket 을 만든 다음에 서버를 기다리게 함
                 logger.info("[CustomWebApplicationServer] client connected.");
+
+//                1. 사용자 요청을 메인쓰레드가 처리하도록 한다
+
+                try(InputStream in = clientSocket.getInputStream();
+                    OutputStream out = clientSocket.getOutputStream()){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+//                    Line by line 으로 읽기 위해 InputStream 을 BufferedReader 로 변환
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while((line = br.readLine()) != ""){
+                        System.out.println(line);
+                    }
+                }
             }
         }
     }
