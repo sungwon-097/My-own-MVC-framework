d438f617ac675ae33f60e296a22c57901639653b
sungwon-097 <csw2757@gmail.com>
2022-10-03 21:45:49 +0900

    Disable SpringBoot annotation

--- src/was-practice/src/main/java/com/example/waspractice/WasPracticeApplication.java
+++ src/was-practice/src/main/java/com/example/waspractice/WasPracticeApplication.java
@@ -1,7 +1,7 @@
 package com.example.waspractice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
