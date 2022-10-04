3036c0ccddaa8a800c79d9e694151d5f8b9d3f49
sungwon-097 <csw2757@gmail.com>
2022-10-04 22:37:20 +0900

    #30 HttpServlet 상속으로 GetMapping 구현
    1. Tomcat이 Servlet의 service() 메소드를 호출함
    2. service() 메소드 내부에서 doGet(req, resp) 을 호출
    3. HTTP GET으로 들어온 요청을 수행함(POST, PUT, PATCH, DELETE등도 동일)

--- src/was-practice/src/main/java/com/example/waspractice/calculator/CalculatorServlet.java
+++ src/was-practice/src/main/java/com/example/waspractice/calculator/CalculatorServlet.java
@@ -7,23 +7,21 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate") // Mapping URI path and current Servlet
-public class CalculatorServlet extends GenericServlet {
+public class CalculatorServlet extends HttpServlet {
     // GenericServlet 은 추상 클래스이기 때문에 Servlet 인터페이스 구현과 달리 필요한 메소드만 구현하면 됨
 
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
     private ServletConfig servletConfig;
 
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-        log.info("init");
-    }
-
-    @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("service");
         int operand1 = Integer.parseInt(request.getParameter("operand1"));
         String operator = request.getParameter("operator");
@@ -34,4 +32,22 @@ public class CalculatorServlet extends GenericServlet {
         PrintWriter writer = response.getWriter();
         writer.println(result);
     }
+
+//    @Override
+//    public void init(ServletConfig servletConfig) throws ServletException {
+//        log.info("init");
+//    }
+
+//    @Override
+//    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+//        log.info("service");
+//        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+//        String operator = request.getParameter("operator");
+//        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+//
+//        int result = Calculator.calculate(new PositiveNumber(operand1),operator ,new PositiveNumber(operand2));
+//
+//        PrintWriter writer = response.getWriter();
+//        writer.println(result);
+//    }
 }
